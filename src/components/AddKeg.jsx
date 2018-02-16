83a94dbae33755dea64b4d535e43bb4f9ec05fc6
Tyler Bussanich <tbussanich@gmail.com>
2018-02-16 10:55:03 -0800

    refactor add keg component to return the value inputted into the form back to the user in the console

--- src/components/AddKeg.jsx
+++ src/components/AddKeg.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
 function AddKeg() {
+  let _name = null;
+  let _brewery = null;
+  let _price = null;
+  let _abv = null;
+  let _ibu = null;
+
+
+  function handleAddKeg(event){
+    event.preventDefault();
+    console.log(_name.value);
+  }
+
   return (
     <div>
       <style jsx>
@@ -21,18 +33,39 @@ function AddKeg() {
         `}
       </style>
       <div className="add">
-        <form className="addForm">
+        <form onSubmit={handleAddKeg} className="addForm">
           <label>Name: </label>
-          <input type="text"></input>
+          <input
+            type='text'
+            id='name'
+            placeholder='Name'
+            ref={(input) => {_name = input;}} />
           <label>Brewery: </label>
-          <input type="text"></input>
+          <input
+            type='text'
+            id='brewery'
+            placeholder='Brewery'
+            ref={(input) => {_brewery = input;}} />
           <label>Price: </label>
-          <input type="text"></input>
+          <input
+            type='text'
+            id='price'
+            placeholder='Price'
+            ref={(input) => {_price = input;}} />
           <label>ABV: </label>
-          <input type="text"></input>
+          <input
+            type='text'
+            id='abv'
+            placeholder='ABV'
+            ref={(input) => {_abv = input;}} />
           <label>IBU: </label>
-          <input type="text"></input>
+          <input
+            type='text'
+            id='ibu'
+            placeholder='IBU'
+            ref={(input) => {_ibu = input;}} />
           <button type="submit">Submit</button>
+
         </form>
       </div>
     </div>
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,8 +11,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      masterKegList: {},
-      selectedKeg: null
+      masterKegList: {}
     };
   }
 
